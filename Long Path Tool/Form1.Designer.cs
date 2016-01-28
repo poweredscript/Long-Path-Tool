5b11d7ec3b069c8ecd710919bfe3aaf620f20f35
poweredscript <poweredscript@gmail.com>
2016-01-28 13:00:09 +0700

    .

---
+++ Long Path Tool/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace Long_Path_Tool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonMove = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonMove
+            // 
+            this.buttonMove.Location = new System.Drawing.Point(12, 38);
+            this.buttonMove.Name = "buttonMove";
+            this.buttonMove.Size = new System.Drawing.Size(75, 23);
+            this.buttonMove.TabIndex = 0;
+            this.buttonMove.Text = "Move";
+            this.buttonMove.UseVisualStyleBackColor = true;
+            this.buttonMove.Click += new System.EventHandler(this.buttonMove_Click);
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Location = new System.Drawing.Point(106, 38);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(75, 23);
+            this.buttonDelete.TabIndex = 1;
+            this.buttonDelete.Text = "Delete";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Item: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(191, 73);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.buttonMove);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Long Path Tool By Pash";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonMove;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Long Path Tool/Form1.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Principal;
+using System.Windows.Forms;
+using IWshRuntimeLibrary;
+using Microsoft.Win32;
+using Ookii.Dialogs;
+
+namespace Long_Path_Tool
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (IsRunAsAdministrator())
+            {
+                RegistyCheck();
+                Close();
+                //Environment.Exit(0);
+                return;
+            }
+            var commandLineArgs = Environment.GetCommandLineArgs().ToList();
+            if (commandLineArgs.Count > 2)
+            {
+                FileOrFolderList.AddRange(commandLineArgs.GetRange(1, commandLineArgs.Count - 1));
+            }
+            else if (commandLineArgs.Count == 2)
+            {
+                FileOrFolderList.Add(commandLineArgs[1]);
+            }
+            InitTimer();
+            //MessageBox.Show(FileOrFolderList.Count.ToString());
+        }
+        private System.Windows.Forms.Timer _timer1;
+        public void InitTimer()
+        {
+            _timer1 = new System.Windows.Forms.Timer();
+            _timer1.Tick += new EventHandler(timer1_Tick);
+            _timer1.Interval = 1000; // in miliseconds
+            _timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Text = @"Item Count: " + FileOrFolderList.Count;
+        }
+        private static readonly List<string> FileOrFolderList = new List<string>();
+        private void buttonMove_Click(object sender, EventArgs e)
+        {
+            string tagetPath = "";
+            using (var dialog = new VistaFolderBrowserDialog())
+            {
+                dialog.SelectedPath = "";
+                dialog.UseDescriptionForTitle = true;
+                dialog.ShowNewFolderButton = true;
+                dialog.Description = @"Select target folder";
+                //dialog.RootFolder = Environment.SpecialFolder.Desktop;
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    tagetPath = dialog.SelectedPath;
+                }
+            }
+            if (Pri.LongPath.Directory.Exists(tagetPath))
+            {
+                foreach (var s in FileOrFolderList)
+                {
+                    if (Pri.LongPath.Directory.Exists(s) || Pri.LongPath.File.Exists(s))
+                    {
+                        MoveTo(s, tagetPath);
+                    }
+                }
+                MessageBox.Show(@"Success", @"Success move " + FileOrFolderList.Count + @" item", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            Close();
+            //Environment.Exit(0);
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            foreach (var s in FileOrFolderList)
+            {
+                if (Pri.LongPath.Directory.Exists(s) || Pri.LongPath.File.Exists(s))
+                {
+                    Delete(s);
+                }
+            }
+            MessageBox.Show(@"Success", @"Success delete " + FileOrFolderList.Count + @" item", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            Close();
+            //Environment.Exit(0);
+        }
+
+
+     //   string _folderName = "c:\\dinoch";
+        //private void buttonBrowse_Click(object sender, EventArgs e)
+        //{
+        //    var browseForFolder = new BrowseForFolder();
+        //    var selectFolder = browseForFolder.SelectFolder("Select a file/folder", "", Handle);
+        //    if (!string.IsNullOrEmpty(selectFolder))
+        //    {
+        //        FileOrFolderList.Add(selectFolder);
+        //    }
+        //}
+
+
+        public void MoveTo(string sourcePath, string tagetPath)
+        {
+            if (IsFolder(sourcePath))
+            {
+                Pri.LongPath.DirectoryInfo directoryInfo = new Pri.LongPath.DirectoryInfo(sourcePath);
+                directoryInfo.MoveTo(Pri.LongPath.Path.Combine(tagetPath, directoryInfo.Name));
+            }
+            else
+            {
+                Pri.LongPath.FileInfo fileInfo = new Pri.LongPath.FileInfo(sourcePath);
+                fileInfo.MoveTo(Pri.LongPath.Path.Combine(tagetPath, fileInfo.Name));
+            }
+        }
+
+
+        public void Delete(string sourcePath)
+        {
+            if (IsFolder(sourcePath))
+            {
+                Pri.LongPath.DirectoryInfo directoryInfo = new Pri.LongPath.DirectoryInfo(sourcePath);
+                directoryInfo.Delete(true);
+            }
+            else
+            {
+                Pri.LongPath.FileInfo fileInfo = new Pri.LongPath.FileInfo(sourcePath);
+                fileInfo.Delete();
+            }
+        }
+        public bool IsFolder(string path)
+        {
+            return Pri.LongPath.Directory.Exists(path);
+        }
+
+        public static void RegistyCheck()
+        {
+            var fullName = System.Reflection.Assembly.GetExecutingAssembly().Location;
+            var thisPath = Directory.GetCurrentDirectory();
+            //var iconPath = Path.Combine(thisPath, "Note.ico");
+            var iconPathReg = "\"" + fullName + "\"" + "";
+            var exePathReg = "\"" + fullName + "\"" + " \"%1\"";
+            var exePathReg2 = "\"" + fullName + "\"" + " \"%V\"";
+            //File
+            Registry.SetValue(@"HKEY_CLASSES_ROOT\*\shell\Long Path Tool", "Icon", iconPathReg, RegistryValueKind.String);
+            Registry.SetValue(@"HKEY_CLASSES_ROOT\*\shell\Long Path Tool\command", "", exePathReg, RegistryValueKind.String);
+            //Folder
+            Registry.SetValue(@"HKEY_CLASSES_ROOT\Directory\shell\Long Path Tool", "Icon", iconPathReg, RegistryValueKind.String);
+            Registry.SetValue(@"HKEY_CLASSES_ROOT\Directory\shell\Long Path Tool\command", "", exePathReg, RegistryValueKind.String);
+            //Inside folder
+            Registry.SetValue(@"HKEY_CLASSES_ROOT\Directory\Background\shell\Long Path Tool", "Icon", iconPathReg, RegistryValueKind.String);
+            Registry.SetValue(@"HKEY_CLASSES_ROOT\Directory\Background\shell\Long Path Tool\command", "", exePathReg2, RegistryValueKind.String);
+
+            var schPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\Microsoft\Windows\SendTo\Long Path Tool.lnk";
+            //MessageBox.Show(fullName);
+            //MessageBox.Show(thisPath);
+            //MessageBox.Show(schPath);
+            //MessageBox.Show(fullName);
+            if (System.IO.File.Exists(schPath)) System.IO.File.Delete(schPath);
+            WshShellClass wsh = new WshShellClass();
+            IWshRuntimeLibrary.IWshShortcut shortcut = wsh.CreateShortcut(schPath) as IWshRuntimeLibrary.IWshShortcut;
+            shortcut.Arguments = "";
+            shortcut.TargetPath = fullName;
+            // not sure about what this is for
+            shortcut.WindowStyle = 1;
+            shortcut.Description = "Long Path Tool";
+            shortcut.WorkingDirectory = thisPath;
+            shortcut.IconLocation = fullName;
+            shortcut.Save();
+            MessageBox.Show("Success Install", "Success Install", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+        private static bool IsRunAsAdministrator()
+        {
+            WindowsIdentity wi = WindowsIdentity.GetCurrent();
+            WindowsPrincipal wp = new WindowsPrincipal(wi);
+
+            return wp.IsInRole(WindowsBuiltInRole.Administrator);
+        }
+    }
+}
